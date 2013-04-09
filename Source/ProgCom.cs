b809d65adc660572cf50cefb4fc6d54a432744d2
dexen deVries <dexen.devries@gmail.com>
2013-04-10 01:06:53 +0200

    bugfix: KSPActionGroup.Custom02...10

--- Source/ProgCom.cs
+++ Source/ProgCom.cs
@@ -231,31 +231,31 @@ public class ProgCom : Part
                 a.ToggleGroup(KSPActionGroup.Custom01);
                 break;
             case 2:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom02);
                 break;
             case 3:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom03);
                 break;
             case 4:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom04);
                 break;
             case 5:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom05);
                 break;
             case 6:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom06);
                 break;
             case 7:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom07);
                 break;
             case 8:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom08);
                 break;
             case 9:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom09);
                 break;
             case 10:
-                a.ToggleGroup(KSPActionGroup.Custom01);
+                a.ToggleGroup(KSPActionGroup.Custom10);
                 break;
             case 11:
                 a.ToggleGroup(KSPActionGroup.Abort);
